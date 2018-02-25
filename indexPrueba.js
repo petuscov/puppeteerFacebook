ad9205ffc47d7fd0b068676b17620859977fa3b8
petuscov <petuscov@gmail.com>
2018-02-25 22:15:52 +0100

    prueba mejorada

--- indexPrueba.js
+++ indexPrueba.js
@@ -14,20 +14,26 @@ const puppeteer = require("puppeteer");
   ).catch(function(err){console.log(err); process.exit(1);});
   const page = await browser.newPage().catch(function(err){console.log(err);process.exit(1);});
   await page.goto('https://www.google.es/').catch(function(err){console.log(err);process.exit(1);});
-  console.log("URL: "+ page.url());
+ 
+  var timestamp1 = new Date();
 
   await page.click(".gsfi[value]").catch(function(err){console.log(err);process.exit(1);});
   await page.keyboard.type("facebook").catch(function(err){console.log(err);process.exit(1);});
  
 
   await page.click('input[type="submit"][jsaction="sf.chk"]').catch(function(err){console.log(err);process.exit(1);});
-  await page.waitForNavigation({timeout: 5000}).catch(function(err){console.log(err);process.exit(1);});
+  await page.waitForNavigation(
+    //{timeout: 5000}
+  ).catch(function(err){console.log(err);process.exit(1);});
   var enlacesBusqueda = await page.$$(".r").catch(function(err){console.log(err);process.exit(1);});
   var enlaceAPulsar = await enlacesBusqueda[1].$("a").catch(function(err){console.log(err);process.exit(1);});
   await enlaceAPulsar.click().catch(function(err){console.log(err);process.exit(1);});
   
-  await page.waitForNavigation({timeout: 5000}).catch(function(err){console.log(err);process.exit(1);});
-  await page.screenshot({path: './pruebaBúsqueda2.png'}).catch(function(err){console.log(err);process.exit(1);});
+  await page.waitForNavigation(
+    //{timeout: 7000}
+  ).catch(function(err){console.log(err);process.exit(1);});
+  await page.screenshot({path: './busquedaEnGoogle.png'}).catch(function(err){console.log(err);process.exit(1);});
+  console.log("tiempo tardado en busqueda: " + ((new Date() - timestamp1)/1000));
   browser.close();
   //mucho más sencillo ir directamente a facebook, pero es una prueba.
 
